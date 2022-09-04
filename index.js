3605c685066664c62e80a9e52965316a107e6549
bautistajm <joel-bautista@outlook.com>
2022-09-04 14:13:30 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// Programa para chequear si el año ingresado es bisiesto. El programa finalizará cuando el usuario ingrese un año bisiesto.
+// let año = 2020
+
+// if ( (0 == año % 4) && (0 != año % 100) || (0 == año % 400) ){
+//     console.log(`${año} es bisiesto`)
+// }else{
+//     console.log(`${año} no es bisiesto`)
+// }
