a58e7cc213a771a0029f50d4c20eff4cdc3a6ab0
bautistajm <joel-bautista@outlook.com>
2022-09-04 14:58:54 -0300

    Clase 3 - Desafio complementariop

--- clase-3-desafio/index.js
+++ clase-3-desafio/index.js
@@ -1,8 +1,18 @@
 // Programa para chequear si el año ingresado es bisiesto. El programa finalizará cuando el usuario ingrese un año bisiesto.
-// let año = 2020
 
-// if ( (0 == año % 4) && (0 != año % 100) || (0 == año % 400) ){
-//     console.log(`${año} es bisiesto`)
-// }else{
-//     console.log(`${año} no es bisiesto`)
-// }
+let anno;
+let continuar = true;
+
+do {
+    anno = parseInt(prompt("Ingrese el año"));
+    if (isNaN(anno)) {
+        alert("Por favor, ingrese un año vaildo.");
+    }else{
+        if ( (0 == anno % 4) && (0 != anno % 100) || (0 == anno % 400) ){
+            alert(`${anno} es bisiesto.`);
+             continuar = false
+         }else{
+             alert(`${anno} no es bisiesto.`);
+         } 
+    }
+}while(continuar)
